888f69d5ef2717ab6be82aaa0f5072af9b91b85d
Sergio Serrano Vargas <ssergio0@hotmail.com>
2018-10-08 10:31:18 -0600

    Migraciones Automáticas

---
+++ ECommerce/ECommerce/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Display(Name ="Departamento")]
+        public string Name { get; set; }
+    }
+}
---
+++ ECommerce/ECommerce/Models/ECommerceContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class ECommerceContext : DbContext
+    {
+        public ECommerceContext() : base("DefaultConnection")
+        {
+        }
+    }
+}
